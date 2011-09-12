e283c952328cee8bbd29e4e600cd7e6a9d97a638
Eric Pearson <epearson@rim.com>
2011-09-12 14:17:57 -0400

    Added comments

--- StarbuckLibrary/BuildServerManager.cpp
+++ StarbuckLibrary/BuildServerManager.cpp
@@ -83,7 +83,8 @@ unsigned short BuildServerManager::start(QString server, int port)
         HANDLE process = OpenProcess(SYNCHRONIZE, FALSE, pid);
         CloseHandle(process);
 #else
-        int process = system("ps axc|awk \"{if ($5==\"node\") print 1}");
+        // run shell script to check if process is running
+        int process = system(ps axc|awk "{if (\$5==\"$1\") print \$1}";
 #endif
         if (process == 0)
         {
